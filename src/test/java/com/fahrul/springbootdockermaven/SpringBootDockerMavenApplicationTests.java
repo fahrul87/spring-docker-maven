713f4dba861aad36a04d25f57fb50aed4d64488e
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-18 21:43:15 +0700

    istimiwir

---
+++ src/test/java/com/fahrul/springbootdockermaven/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrul.springbootdockermaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
